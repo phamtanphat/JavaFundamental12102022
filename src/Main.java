418232cbaa76c12a80917804935fb2b17e647069
Phat <phatdroid94@gmail.com>
2022-10-19 19:07:05 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        // Câu lệnh in ra hệ thống
+        // System.out.println("Hello Word");
+
+        // 1: Kiểu dữ liệu
+        // Kiểu dữ liệu nguyên thủy (Primitive)
+        // Ex: char, int, long, float, double, boolean
+        // Kiểu dữ liệu đối tượng (Object)
+        // Ex: String, Integer, Long, Float, Double, Boolean
+
+        // 2: Khai báo biến
+        char word = 'a';
+        int number1 = -1;
+        float number2 = 1.2f;
+        boolean isOnline = true;
+
+    }
+}
+
+
