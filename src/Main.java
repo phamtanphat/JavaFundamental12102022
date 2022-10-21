c1cd892592b696df511135404faa904a6bc3bc31
Phat <phatdroid94@gmail.com>
2022-10-21 18:06:28 +0700

    toan tu

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,48 @@
 public class Main {
     public static void main(String[] args) {
-        // Câu lệnh in ra hệ thống
-        // System.out.println("Hello Word");
+//         Câu lệnh in ra hệ thống
+//         System.out.println("Hello Word");
+//
+//         1: Kiểu dữ liệu
+//         Kiểu dữ liệu nguyên thủy (Primitive)
+//         Ex: char, int, long, float, double, boolean
+//         Kiểu dữ liệu đối tượng (Object)
+//         Ex: String, Integer, Long, Float, Double, Boolean
+//
+//         2: Khai báo biến
+//        char word = 'a';
+//        int number1 = -1;
+//        float number2 = 1.2f;
+//        boolean isOnline = true;
 
-        // 1: Kiểu dữ liệu
-        // Kiểu dữ liệu nguyên thủy (Primitive)
-        // Ex: char, int, long, float, double, boolean
-        // Kiểu dữ liệu đối tượng (Object)
-        // Ex: String, Integer, Long, Float, Double, Boolean
+//         tên, tuoi, so thich, chieucao
+//        String name = "Phạm Tấn Phát";
+//        int age = 28;
+//        String hobby = "code";
+//        float height = 1.75f;
+//        System.out.println(name);
+//        System.out.println(age);
+//        System.out.println(hobby);
+//        System.out.println(height);
 
-        // 2: Khai báo biến
-        char word = 'a';
-        int number1 = -1;
-        float number2 = 1.2f;
-        boolean isOnline = true;
+//          3: Toán tử
+//        int a = 10;
+//        int b = 5;
+//        int result = b % a;
+//        System.out.println(result);
 
+        int a = 10;
+        int b = a++;
+
+        // Toán tử ++
+        // Th1: a++
+        // 1: Gán giá trị vào cho a
+        // 2: Tăng a lên 1 đơn vị
+        // Th2: ++a
+        // 1: Tăng a lên 1 đơn vị
+        // 2: Gán giá trị vào cho a
+        System.out.println(b);
+        System.out.println(a);
     }
 }
 
